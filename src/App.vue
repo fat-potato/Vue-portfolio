520c9f91cdcba7a195a4d2daab83cb7bf7898c35
Khoi Nguyen Minh <barney@Khois-MacBook-Pro.local>
2017-11-28 01:15:10 +0700

    Basic setup for portfolio

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+require('material-design-icons/iconfont/material-icons.css')
+require ('font-awesome/css/font-awesome.css')
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+  overflow-x: hidden;
+}
+</style>
